254a3da5ab5a722de831ffb84a4854c759a7d545
psalvi03 <psalvi@logilite.com>
2021-06-06 15:44:50 +0530

    Proj1-Commit-First

---
+++ HBProj01-SavingObject/src/com/nt/entity/Product.java
@@ -0,0 +1,48 @@
+//entity class/ modal class/persistent class/Domain class/POJO class/HLO(Hibernate Lungage Object)
+package com.nt.entity;
+
+import java.io.Serializable;
+
+public class Product implements Serializable{
+	//bean properties
+	private Integer pid;
+	private String pname;
+	private Float price;
+	private Float qty;
+
+	//setter and getter method
+
+	public Integer getPid() {
+		return pid;
+	}
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	public Float getQty() {
+		return qty;
+	}
+	public void setQty(Float qty) {
+		this.qty = qty;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Product [pid=" + pid + ", pname=" + pname + ", price=" + price + ", qty=" + qty + "]";
+	}
+	
+		
+
+}
---
+++ HBProj01-SavingObject/src/com/nt/test/PersistObjectTest.java
@@ -0,0 +1,56 @@
+package com.nt.test;
+
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.nt.entity.Product;
+
+public class PersistObjectTest {
+
+	public static void main(String[] args) {
+			Configuration cfg=null;
+			SessionFactory factory=null;
+			Session ses=null;
+			Product prod=null;
+			Transaction tx=null;
+			boolean flag=false;
+		   int idVal=0;
+			//Activate hibernate frameWork
+		cfg=new Configuration();
+		//supply hbrnate cfg file as input file
+		cfg.configure("com/nt/cfgs/hibernate.cfg.xml");
+		//build session factory
+		factory=cfg.buildSessionFactory();
+		//open session
+		ses=factory.openSession();
+		// create entity object to save with db s/w
+		prod=new Product();
+		prod.setPid(113); prod.setPname("Chair"); prod.setPrice(2500.0f);prod.setQty(15.0f);
+		try {
+			tx=ses.beginTransaction();  //internall call con.setAutoCommit(false) to begin the Tx
+			//save object
+			ses.persist(prod);
+			flag=true;
+		}//try
+		catch(HibernateException he)
+		{
+			he.printStackTrace();
+			flag=false;
+		}//catch
+		finally {
+			//commit or rollback tx
+			if(flag=true) {
+				tx.commit();  //internally call con.commit
+			System.out.println("Object(Data) is saved");
+			}
+			else {
+				tx.rollback();  //internally calls con.rollback
+		  	System.out.println("Data is not saved");
+		  	}
+		}//finally
+	}//main
+}//class
